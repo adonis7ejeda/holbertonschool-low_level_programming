d88b48cdf8560dbd2bc086cd6315ddfb10fec1b0
adonis7ejeda <adonis7ejeda@gmail.com>
2020-02-06 21:25:35 +0000

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+if (a != 57)
+putchar(44);
+putchar(32);
+}
+
+putchar('\n');
+
+return (0);
+}
