634234f32d1781f703c9249b21860d2c86d94f01
adonis7ejeda <adonis7ejeda@gmail.com>
2020-02-14 03:21:10 +0000

    I feel like I am

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line of length n
+ * @n: length of diagonal
+ *
+ * Return: void
+ */
+
+
+void print_diagonal(int n)
+{
+
+int a, b;
+
+if (n <= 10)
+{
+_putchar('\n');
+}
+
+for (a = 0; a < n; a++)
+{
+
+for (b = 0; b < a; b++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+
+}
