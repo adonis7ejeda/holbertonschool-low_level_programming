c4fa9a06621af8a02a2a06a5e21f9e93ffb40d5a
adonis7ejeda <adonis7ejeda@gmail.com>
2020-02-21 04:39:53 +0000

    root13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,29 +1,27 @@
 #include "holberton.h"
 
 /**
- * rot13 - conver string o rot13
- * @s: string
- * Return: string
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
  */
-
 char *rot13(char *s)
 {
-int i;
+int i, j;
+char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-i = 0;
-while (s[i] != '\0')
+for (i = 0; *(s + i); i++)
 {
-if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+for (j = 0; j < 52; j++)
 {
-s[i] = (s[i] + 13);
+if (a[j] == *(s + i))
+{
+*(s + i) = b[j];
+break;
 }
-else
-while ((s[i] >= 'n' && s[i] <= 'z') ||
-(s[i] >= 'N' && s[i] <= 'Z'))
-{
-s[i] = (s[i] - 13);
 }
-i++;
 }
 return (s);
 }
