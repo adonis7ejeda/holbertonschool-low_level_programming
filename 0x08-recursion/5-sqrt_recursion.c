647920139201cfc45a05b54774e791db5ca86756
adonis7ejeda <adonis7ejeda@gmail.com>
2020-02-27 05:21:41 +0000

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * comp - evalue result
+ * @n: value
+ * @aux: auxiliar
+ * Return: aux
+ */
+
+int comp(int aux, int n)
+{
+if (aux * aux == n)
+return (aux);
+
+if (aux * aux > n)
+return (-1);
+
+return (comp(aux + 1, n));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: value
+ * Return: result
+ */
+
+int _sqrt_recursion(int n)
+{
+return (comp(1, n));
+}
