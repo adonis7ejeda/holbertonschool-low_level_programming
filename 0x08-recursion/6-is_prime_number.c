3f48b54bc9b9fa3f70da74f7ec3bb159e0148dc3
adonis7ejeda <adonis7ejeda@gmail.com>
2020-02-27 21:43:16 +0000

    Inception. Is it possible

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * mod - evalue factors of a number.
+ * @n: number
+ * @aux: factor
+ * Return: 0 - 1
+ */
+
+int mod(int aux, int n)
+{
+int sum = 0;
+
+if (aux <= n)
+{
+if (n % aux == 0)
+sum++;
+
+return (sum + mod(aux + 1, n));
+}
+return (sum);
+}
+
+/**
+ * is_prime_number - evalue prime number.
+ * @n: number
+ * Return: 0 - 1
+ */
+
+int is_prime_number(int n)
+{
+if (mod(1, n) == 2)
+return (1);
+
+return (0);
+}
