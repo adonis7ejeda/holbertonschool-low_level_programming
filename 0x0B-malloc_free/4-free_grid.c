c3c571e4e6f7fbc3fea332249485665117d9b070
adonis7ejeda <adonis7ejeda@gmail.com>
2020-03-03 23:12:01 +0000

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - returns a pointer to a 2 dimensional array of integers.
+ * @grid: matrix
+ * @height: height of the grid
+ *
+ * Return: pointer.
+ */
+
+void free_grid(int **grid, int height)
+{
+
+int i;
+
+for (i = 0; i < height; i++)
+free(grid[i]);
+
+free(grid);
+}
