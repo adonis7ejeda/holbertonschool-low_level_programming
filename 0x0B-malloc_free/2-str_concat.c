17f29246bd769f15899de62c0f701286b203d53c
adonis7ejeda <adonis7ejeda@gmail.com>
2020-03-03 21:49:53 +0000

    Task 2.1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,23 +14,17 @@ char *str_concat(char *s1, char *s2)
 unsigned int i, j, k, l;
 char *s;
 
-if (s1 == NULL || s2 == NULL)
-{
-
+if (s1 == NULL)
 i = 0;
-j = 0;
-
-}
 else
-{
-
 for (i = 0; s1[i]; i++)
 ;
 
+if (s2 == NULL)
+j = 0;
+else
 for (j = 0; s2[j]; j++)
 ;
-
-}
 
 k = i + j + 1;
 
