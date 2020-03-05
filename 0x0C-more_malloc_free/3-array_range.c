fba2ed9458bde702d61c50ba6e31f21cac2bdd7c
adonis7ejeda <adonis7ejeda@gmail.com>
2020-03-05 21:50:51 +0000

    Task 3.5

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -23,8 +23,8 @@ a = malloc(sizeof(int) * (max - min + 1));
 if (a == NULL)
 return (NULL);
 
-for (i = 0; i <= max; i++, min++)
-a[i] = min;
+for (i = 0; i <= max; i++)
+a[i] = min++;
 
 return (a);
 }
