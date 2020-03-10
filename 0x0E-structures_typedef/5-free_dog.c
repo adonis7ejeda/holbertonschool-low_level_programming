9ebda70ace20c80c763e53ac33a21354f7b6cb47
adonis7ejeda <adonis7ejeda@gmail.com>
2020-03-10 04:44:51 +0000

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - free dog.
+ * @d: dog structure
+ * Return: ntg
+ */
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
