e536ec968a336eee4e9768210456da99e2abed23
adonis7ejeda <adonis7ejeda@gmail.com>
2020-03-20 23:12:10 +0000

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,20 @@
 #include "lists.h"
+
 /**
  * free_list - function that frees a list_t list.
  * @head: pointer to header
  * Return: ntg
  */
+
 void free_list(list_t *head)
 {
 list_t *aux;
 
 while (head != NULL)
+{
 aux = head->next;
 free(head->str);
 free(head);
 head = aux;
-
+}
 }
