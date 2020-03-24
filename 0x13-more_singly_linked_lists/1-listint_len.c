8e637a770b707a00881300ccdc14fa7730040389
adonis7ejeda <adonis7ejeda@gmail.com>
2020-03-24 21:31:29 +0000

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - return num of the elements.
+ * @h: head pointer.
+ * Return: # of elements.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t nodes = 0;
+
+while (h != NULL)
+{
+h = h->next;
+nodes++;
+}
+return (nodes);
+}
