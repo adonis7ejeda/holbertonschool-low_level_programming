52b02361fafb1e4a601cfe08d04d133d0db97472
adonis7ejeda <adonis7ejeda@gmail.com>
2020-03-24 22:31:32 +0000

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list.
+ * @head: head pointer.
+ * Return: ntg.
+ */
+void free_listint(listint_t *head)
+{
+listint_t *aux;
+
+while (head != NULL)
+{
+aux = head->next;
+free(head);
+head = aux;
+}
+}
