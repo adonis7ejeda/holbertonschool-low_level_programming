54d493394794280d8cf68e8a6e6630b634f07e7b
adonis7ejeda <adonis7ejeda@gmail.com>
2020-05-30 03:49:13 +0000

    Task 6.1

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -17,9 +17,9 @@ void hash_table_delete(hash_table_t *ht)
 		while (current != NULL)
 		{
 			prev = current;
-			current = current->next;
 			free(prev->key);
 			free(prev->value);
+			current = current->next;
 			free(prev);
 		}
 	}
